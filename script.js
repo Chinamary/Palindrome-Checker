9e76bc13e3619027a3c6114ce0b32a41f0ef8ae8
Favour Ukonu <favouroukonu@gmail.com>
2024-03-26 19:04:24 +0100

    Fix: Result text color

--- script.js
+++ script.js
@@ -9,11 +9,25 @@ form.addEventListener('submit', function (e) {
   const inputValue = input.value;
   // console.log(inputValue.split('').reverse().join(''));
 
-  const reversedString = inputValue.split('').reverse().join('');
+
+  const inputToLowerCaseString = inputValue.toLowerCase().replace(/[^a-z0-9]/g, '');
+  //toLowerCase() is used to convert the string to lowercase, replace() is used to replace the character with another character
+  console.log(inputToLowerCaseString);
+
+  const reversedString = inputToLowerCaseString.split('').reverse().join('');
   //split() is used to split the code, the reverse() is used to reverse the word while the join() is used to join back the reversed word
-  if (reversedString === inputValue) {//Here, if reversedString is strictly equals to inputValue show the .result_text
-    document.querySelector(".result__text").textContent = reversedString + " is a palindrome word ✔";
-  } else {//If not show this one
-    document.querySelector(".result__text").textContent = reversedString + " is not a palindrome word ❌";
+
+  // Select the result text
+  const resultText = document.querySelector(".result__text")
+  if (reversedString === inputToLowerCaseString) {
+    //Here, if reversedString is strictly equals to inputToLowercaseString show the .result_text
+    resultText.textContent = inputValue + " is a palindrome word ✔";
+    resultText.style.color = "green";
+
+  } else {
+    //If not show this one
+
+    resultText.textContent = inputValue + " is not a palindrome word ❌";
+    resultText.style.color = "red";
   }
 });
