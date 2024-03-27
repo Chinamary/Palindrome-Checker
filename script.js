dd79a6c9d299652bfd8c0eaa36d7542d4b555d1b
Chinamary <chinamary2005@gmail.com>
2024-03-26 23:03:21 -1100

    Form Validation

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ form.addEventListener('submit', function (e) {
   // console.log(inputValue.split('').reverse().join(''));
 
 
-  const inputToLowerCaseString = inputValue.toLowerCase().replace(/[^a-z0-9]/g, '');
+  const inputToLowerCaseString = inputValue.toLowerCase().replace(/[^a-z0-9]/gi, '');
   //toLowerCase() is used to convert the string to lowercase, replace() is used to replace the character with another character
   console.log(inputToLowerCaseString);
+
 
   const reversedString = inputToLowerCaseString.split('').reverse().join('');
   //split() is used to split the code, the reverse() is used to reverse the word while the join() is used to join back the reversed word
@@ -26,8 +27,16 @@ form.addEventListener('submit', function (e) {
 
   } else {
     //If not show this one
-
     resultText.textContent = inputValue + " is not a palindrome word ❌";
     resultText.style.color = "red";
   }
+
+  //Display if a text is not added to the input box
+  if (!inputToLowerCaseString) {
+    resultText.textContent = "Please enter a text";
+    resultText.style.color = "red";
+    return false;
+  }
 });
+
+
